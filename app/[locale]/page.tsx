d5f3d9c67e07475b41b95e455bc096cd69b91fd9
beka <badagadzeb@gmail.com>
2025-11-15 11:59:57 +0400

    add nextIntl

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,8 +1,26 @@
 import Image from "next/image";
+import { getTranslations, setRequestLocale } from "next-intl/server";
+import LanguageSelector from "@/components/languageSelector/languageSelector";
+import { routing } from "@/i18n/routing";
 
-export default function Home() {
+export default async function Home({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  setRequestLocale(locale);
+
+  const t = await getTranslations({ locale });
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <LanguageSelector defaultValue={locale} label="Language">
+        {routing.locales.map((locale: string) => (
+          <option key={locale} value={locale}>
+            {locale.toUpperCase()}
+          </option>
+        ))}
+      </LanguageSelector>
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
@@ -14,7 +32,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            {t("title")}
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
