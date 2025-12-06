2ca84ee615f07c263c52662192eadb10846354de
beka <badagadzeb@gmail.com>
2025-12-06 12:59:34 +0400

    nth

--- features/about/aboutBanner.tsx
+++ features/about/aboutBanner.tsx
@@ -10,7 +10,7 @@ export default function AboutBanner() {
     <div className="lg:col h-[500px] bg-[url('/imgs/VectorMob.png')] bg-cover bg-center md:mt-0 md:h-[300px] md:bg-[url('/imgs/Vector.png')]">
       <div className="customContainer flex h-full items-center justify-center lg:justify-start">
         <div className="pt-[210px] text-center lg:pt-0 lg:pl-[160px]">
-          <h1 className="font-bpg-web-002-caps text-2xl text-white">
+          <h1 className="font-bpg-web-002-caps mb-2 text-2xl text-white">
             &quot;EAGLE POWER TOOLS&quot;{" "}
           </h1>
           <h2 className="mx-auto mt-2 mb-8 max-w-[272px] leading-8 text-white lg:max-w-full">
