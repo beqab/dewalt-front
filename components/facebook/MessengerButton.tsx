63dde958f36e3a79991eabf45942194c8ac407e2
beka <badagadzeb@gmail.com>
2026-03-20 00:04:48 +0400

    remove outline from fbMesenger button

--- components/facebook/MessengerButton.tsx
+++ components/facebook/MessengerButton.tsx
@@ -17,7 +17,7 @@ export default function MessengerButton({
       target="_blank"
       rel="noopener noreferrer"
       aria-label={label}
-      className="fixed right-6 bottom-6 z-50 flex h-16 w-16 items-center justify-center rounded-full p-2 shadow-lg transition-transform hover:scale-105 focus-visible:outline-2 focus-visible:outline-offset-2"
+      className="fixed right-6 bottom-6 z-50 flex h-16 w-16 items-center justify-center rounded-full p-2 transition-transform hover:scale-105 focus-visible:outline-2 focus-visible:outline-offset-2"
     >
       <span className="relative inline-flex items-center justify-center before:absolute before:h-[30px] before:w-[30px] before:rounded-full before:bg-[#fff] before:content-['']">
         <Image
